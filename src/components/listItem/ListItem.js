c9fa14326d87a82b5ff35a1e1299b49cec8f1cf7
Nisha Kumari <62687310+nishakumari13@users.noreply.github.com>
2022-04-02 21:48:34 +0530

    listItem comment

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -22,6 +22,7 @@ export default function ListItem({ index }) {
         src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
         alt=""
       />
+          // show after movie is hovered
       {isHovered && (
         <>
           <video src={trailer} autoPlay={true} loop />
